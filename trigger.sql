54e1273f46abeaf424c50515c76af993503d9d8c
shiva1788 <111282656+shiva1788@users.noreply.github.com>
2024-12-14 18:30:04 +0530

    Add files via upload

--- trigger.sql
+++ trigger.sql
@@ -7,24 +7,36 @@ wor_h_s INT,
 tot_s int
 );
 
-INSERT INTO trigg(id,NAME,per_h_s,wor_h_s) VALUES(3,"pri",500,7)
-SELECT * FROM trigg;
+#INSERT INTO trigg(id,NAME,per_h_s,wor_h_s) VALUES(3,"pri",500,7)
+#SELECT * FROM trigg;
 
 #TRUNCATE TABLE trigg;
 # we can insert the data before using 2 column that can insert in another column
+#delimiter //
+#CREATE TRIGGER shi_vam
+#BEFORE INSERT 
+#ON trigg for EACH ROW
+#BEGIN
+#set NEW.tot_s = NEW.per_h_s * NEW.wor_h_s;
+#END; //
+#delimiter ;
+#SELECT * FROM trigg;
+
+
+#update trigger
 delimiter //
-CREATE TRIGGER shi_vam
-BEFORE INSERT 
-ON trigg for EACH ROW
-begin
-set NEW.tot_s = NEW.per_h_s * NEW.wor_h_s;
+CREATE TRIGGER shi_
+BEFORE UPDATE 
+ON trigg for EACH ROW 
+BEGIN
+SET NEW.tot_s = NEW.per_h_s * NEW.wor_h_s;
 END; //
-delimiter ;
+delimiter;
 SELECT * FROM trigg;
+UPDATE trigg SET per_h_s = 600 WHERE id = 1;
 
 
 #how we can use two table in triggers
-
 DELIMITER $$
 
 CREATE TRIGGER calculate_amount_before_insert
@@ -39,8 +51,48 @@ BEGIN
     WHERE product_id = NEW.product_id;  -- assuming the product_id is the linking field
 
     -- Calculate and set the amount
-    SET NEW.amount = product_rate * NEW.quantity;
+    SET NEW.amount = product_rate * NEW.quantity WHERE product_id = NEW.product_idl;
 END $$
 
 DELIMITER ;
+
+
+
+CREATE TABLE ash(
+product_id INT,
+quantity INT ,
+amount INT 
+);
+INSERT INTO ash(product_id,quantity) VALUES(1,2);
+TRUNCATE TABLE ash;
+SELECT * FROM ash;
+SELECT * FROM ashh;
+
+SELECT * FROM ash;
+CREATE TABLE ashh(
+product_id1 INT,
+rate INT  
+);
+INSERT INTO ash(product_id , quantity ) VALUES(4,2 );
+
+DROP TABLE ashh;
+INSERT INTO ashh(product_id1,rate) VALUES(4,100)
+SELECT * FROM ashh;
+SELECT * FROM ash;
+
+
+delimiter //
+CREATE TRIGGER tr_add
+after INSERT
+ON ash FOR EACH ROW
+BEGIN
+
+UPDATE ash SET 
+amount =  ashh.rate * NEW.quantity WHERE ash.product_id=ashh.product_id1;
+
+END // 
+delimiter;
+#DROP TRIGGER hash
+
+
 
